d13a269a1d11559730548b5cac1a7c3c2151c287
xialisheng <Xls080910@163.com>
2023-07-17 11:19:15 +0800

    home-banner轮播图

--- src/views/Home/components/HomeBanner.vue
+++ src/views/Home/components/HomeBanner.vue
@@ -1,8 +1,48 @@
+<script setup>
+import { getBannerAPI} from '@/apis/home'
+import { ref,onMounted } from 'vue'
+
+const bannerList = ref([])
+
+const getBannerList = async() =>{
+  const res = await getBannerAPI()
+  // console.log(res);
+  bannerList.value = res.result
+  // console.log(bannerList.value);
+}
+
+onMounted(()=>{
+  getBannerList()
+})
+
+</script>
+
+
+
 <template>
-  <div>我是banner</div>
+  <div class="home-banner">
+    <el-carousel height="500px">
+      <el-carousel-item v-for="item in bannerList" :key="item.id">
+        <img :src="item.imgUrl" alt="">
+      </el-carousel-item>
+    </el-carousel>
+  </div>
 </template>
 
-<script lang='ts' setup>
-</script>
+
+
 <style scoped lang='scss'>
+.home-banner {
+  width: 1240px;
+  height: 500px;
+  position: absolute;
+  left: 0;
+  top: 0;
+  z-index: 98;
+
+  img {
+    width: 100%;
+    height: 500px;
+  }
+}
 </style>

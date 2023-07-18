e7b0ba1046247c29bd4f44c22c2994cbf8ec38a0
xialisheng <Xls080910@163.com>
2023-07-18 14:18:45 +0800

    路由缓存

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -4,12 +4,13 @@ import { getBannerAPI } from '@/apis/home'
 import { ref, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
 import GoodsItem from '../Home/components/GoodsItem.vue';
+import { onBeforeRouteUpdate } from 'vue-router'
 
 const route = useRoute()
 const categoryData = ref({})
 
-const getCategoryData = async () => {
-  const res = await getCategoryAPI(route.params.id)
+const getCategoryData = async (id = route.params.id) => {
+  const res = await getCategoryAPI(id)
   // console.log(res);
   categoryData.value = res.result
 }
@@ -29,6 +30,14 @@ onMounted(() => {
   getCategoryData(),
     getBannerList()
 })
+
+// 目标：路由参数变化的时候 可以把分类数据的接口重新发送
+onBeforeRouteUpdate((to) =>{
+  console.log('路由变化了');
+  console.log(to);
+  getCategoryData(to.params.id)
+})
+
 </script>
 
 <template>
--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -10,12 +10,17 @@ const categoryStore = useCategoryStore()
 onMounted(() => {
   categoryStore.getCategory()
 })
+
+
+
 </script>
 
 <template>
   <LayoutFixed/>
   <LayoutNav />
   <LayoutHeader />
+  <!-- 添加key 破坏复用机制 强制销毁重建 -->
+  <!-- <RouterView :key="$route.fullPath"/> -->
   <RouterView />
   <LayoutFooter />
 </template>
